0d13638d50f1979725900f7bcf43e5488202dbc8
Krishna Sharma <rakesh7355249204@gmail.com>
2025-04-03 20:24:07 +0530

    CloudberryJam

---
+++ EducationalCodeforcesRound177(Div.2)/CloudberryJam.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+using namespace std;
+#define ll long long 
+#define vint vector<int>
+#define vll vector<long long>
+#define vchar vector<char>
+#define rr(x) x.begin(),x.end()
+#define nl endl
+#define str string
+#define si set<int>
+#define pb(x) push_back(x)
+#define ull unsigned long long 
+#define for(i,a,b) for(int i=a;i<b;++i)
+#define sp " "
+
+void logic(){
+    int n;
+    cin >> n;
+
+    cout << n*2 << nl;
+}
+
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        logic();
+    }
+}
