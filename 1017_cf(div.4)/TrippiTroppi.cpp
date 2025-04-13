6f87e81909d3efe327bcc711de3adc7989a5602d
Krishna Sharma <rakesh7355249204@gmail.com>
2025-04-13 22:31:07 +0530

    TrippiTroppi

---
+++ 1017_cf(div.4)/TrippiTroppi.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <map>
+using namespace std;
+#define ll long long 
+#define vint vector<int>
+#define vll vector<long long>
+#define vchar vector<char>
+#define vstr vector<string>
+#define rr(x) x.begin(),x.end()
+#define nl endl
+#define str string
+#define si set<int>
+#define pb(x) push_back(x)
+#define ulll unsigned long long 
+#define floop(i,a,b) for(int i=a;i<b;++i)
+#define sp " "
+#define mii map<int,int>
+#define fauto(i,x) for(auto i:x)
+
+void logic(){
+    str s,s1,s2;
+    cin >> s >> s1 >> s2;
+    cout << s[0] << s1[0] << s2[0] << endl;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        logic();
+    }
+    return 0;
+}
