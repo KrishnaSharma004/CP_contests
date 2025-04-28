7d06024ebc8595e004304849eb147a988ff66cde
Krishna Sharma <rakesh7355249204@gmail.com>
2025-04-28 21:05:11 +0530

    CardGame

---
+++ EducationalCodeforcesRound178(div.2)/CardGame.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <map>
+#include <cstring>
+#include <unordered_map>
+using namespace std;
+#define ll long long 
+#define vint vector<int>
+#define vll vector<long long>
+#define vchar vector<char>
+#define vstr vector<string>
+#define rr(x) x.begin()+1,x.end()
+#define nl endl
+#define str string
+#define si set<int>
+#define pb(x) push_back(x)
+#define ull unsigned long long 
+#define floop(i,a,b) for(int i=a;i<b;++i)
+#define sp " "
+#define mii map<int,int>
+#define fauto(i,x) for(auto i:x)
+#define vpint vector<pair<int,int> >
+#define umpint unordered_map<int,int>
+#define vvint vector<vector<int> > 
+#define maxelem(x) *max_element(x.begin(),x.end())
+#define minelem(x) *min_element(x.begin(),x.end())
+void logic(){
+    int n;
+    str s;
+    cin >> n;
+    cin >> s;
+    vint a,b;
+    a.reserve(n);
+    b.reserve(n);
+    floop(i,0,n){
+        if(s[i]== 'A'){
+            a.pb(i+1);
+        }else{
+            b.pb(i+1);
+        } 
+    }
+    bool a_win = true;
+    for(int x:a){
+        bool b_beat = true;
+        if(x==1){
+            for(int y:b){
+                if(2<=y && y <= n-1){
+                    b_beat = false;
+                    break;
+                }
+            }
+        }
+        else if(x==n){
+            for(int y:b){
+                if(y==1){
+                    b_beat = false;
+                    break;
+                }
+            }
+        }else{
+            for(int y:b){
+                if(y > x){
+                    b_beat = false;
+                    break;
+                }
+            }
+        }
+        if(b_beat){
+            a_win = false;
+            break;
+        }
+    }
+    if(!a_win) cout << "Alice" << nl;
+    else cout << "Bob" <<  nl;
+}
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        logic();
+    }
+    return 0;
+}
