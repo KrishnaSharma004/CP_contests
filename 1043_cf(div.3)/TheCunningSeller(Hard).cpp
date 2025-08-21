1b67a4027e6c96746d6baa22838e7a455fa08270
Krishna Sharma <rakesh7355249204@gmail.com>
2025-08-21 21:30:00 +0530

    ArithmeticCompetition Homework TheCunningSeller(easy) TheCunningSeller(Hard) TheSecretNumber

---
+++ 1043_cf(div.3)/TheCunningSeller(Hard).cpp
@@ -0,0 +1,210 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <map>
+#include <cstring>
+#include <unordered_map>
+#include <deque>
+#include <numeric>
+#include <algorithm>
+#include <set>
+#include <queue>
+#include <iomanip>
+#include <cstdint>
+using namespace std;
+#define ll long long 
+#define pint pair<int,int>
+#define vint vector<int>
+#define vll vector<long long>
+#define vchar vector<char>
+#define vstr vector<string>
+#define vbool vector<bool>
+#define vtiii vector<tuple<int,int,int> >
+#define as(x) x.begin(),x.end()
+#define ds(y) y.rbegin(),y.rend()
+#define nl endl
+#define str string
+#define si set<int>
+#define ull unsigned long long 
+#define floop(i,a,b) for(int i=a;i<b;++i)
+#define sp " "
+#define mint map<int,int>
+#define mll map<ll,ll>
+#define fauto(i,x) for(auto &i:x)
+#define vpint vector<pair<int,int> >
+#define vpll vector<pair<ll,ll> >
+#define umpint unordered_map<int,int>
+#define vvint vector<vector<int> > 
+#define vvll vector<vector<ll> > 
+#define vpib vector<pair<int,bool> >
+#define maxelem(x) *max_element(x.begin(),x.end())
+#define minelem(x) *min_element(x.begin(),x.end())
+#define MOD (ll)(1e9+7)
+bool check(ll mid,vll &hg,ll x){
+    ll units=0;
+    int n=hg.size();
+    floop(i,0,n){
+        if(hg[i]<mid){
+            units += (mid-hg[i]);
+        }
+    }
+    return units<=x;
+}
+vint FindMax(vint &arr){
+    vpint tmp(arr.size());
+
+    floop(i,0,tmp.size()){
+        tmp[i].first=arr[i];
+        tmp[i].second=i;
+    }
+    sort(ds(tmp));
+    vint ans(3);
+    floop(i,0,3) ans[i]=tmp[i].second;
+    return ans;
+}
+ll lcm(ll num1,ll num2){
+    return (num1*num2)/(gcd(num1,num2));
+}
+ll calculatesum(ll start,ll end){
+    ll sum = ((start +end)*(end-start +1))/2;
+
+    return sum;
+}
+const ll mod = 1e9+7;
+bool pf(ll val,int ind,int x,int mid){
+    if(val + (1LL * (ind + 1)*(mid-1))<=x)
+        return true;
+    return false;
+}
+int binsearch(ll val,int ind,int x){
+    int low = 1,high=1e9+5;
+    int ans = 0;
+
+    while(low<=high){
+        int mid = (low + high)/2;
+        if(pf(val,ind,x,mid)){
+            ans=mid;
+            low=mid+1;
+        }else{
+            high = mid -1;
+        }
+    }
+    return ans ;
+}
+int binarysearch(vll &pmax,int n,int val){
+    int low = 0,high=n-1;
+    int ans = -1;
+
+    while(low<=high){
+        int mid = (low + high)/2;
+        if(pmax[mid]<=val){
+            ans=mid;
+            low=mid+1;
+        }else{
+            high = mid -1;
+        }
+    }
+    return ans ;
+}
+ll modl = 998244353;
+int ans=0;
+int dfs(int node,vvint &adj,str &s){
+    if(adj[node].size()==0){
+        return s[node-1]=='W'?1:-1;
+    }
+    int cnt = 0;
+    for(auto child:adj[node]){
+        cnt +=dfs(child,adj,s);
+    }
+    cnt += (s[node-1]=='W'?1:-1);
+    if(cnt==0) ans++;
+
+    return cnt;
+}
+ll calculateSumOfFirstX(int x){
+    return x*1ll*(x+1)/2;
+}
+ll ceil_division(ll a,ll b){
+    return (a+b-1)/b;
+}
+int segmentStart[200000], segmentEnd[200000];
+
+bool isReachableWithJump(int segmentCount, int maxJump)
+{
+    int currentMinPosition = 0;
+    int currentMaxPosition = 0;
+
+    for (int i = 0; i < segmentCount; i++)
+    {
+        currentMaxPosition += maxJump;
+        currentMinPosition -= maxJump;
+
+        int reachableStart = max(currentMinPosition, segmentStart[i]);
+        int reachableEnd = min(currentMaxPosition, segmentEnd[i]);
+
+        if (reachableStart > reachableEnd)
+        {
+            return false;
+        }
+
+        currentMinPosition = reachableStart;
+        currentMaxPosition = reachableEnd;
+    }
+
+    return true;
+}
+vector<ull> pow3(40, 1);
+void prepare_pow3() {
+    for (int i = 1; i < 40; ++i) {
+        pow3[i] = pow3[i - 1] * 3;
+    }
+}
+uint64_t base3_sum(uint64_t z) {
+    uint64_t r = 0;
+    for (; z; z /= 3) r += z % 3;
+    return r;
+}
+void logic() {
+    int64_t a, b;
+    cin >> a >> b;
+
+    int sb3 = base3_sum(a);
+    int64_t lim = min(a, b);
+    int64_t d = (lim % 2 == a % 2) ? lim : lim - 1;
+
+    if (d < sb3) {
+        cout << -1 << nl;
+        return;
+    }
+
+    int64_t rem = (a - d) / 2;
+    vector<uint64_t> c(1, a);
+    uint64_t extra = 0;
+    size_t i = 0;
+
+    while (rem > 0) {
+        if (i >= c.size()) c.push_back(0);
+        uint64_t q = c[i] / 3;
+        if (q == 0) { ++i; continue; }
+
+        uint64_t take = min<uint64_t>(q, rem);
+        extra += take * pow3[i];
+        c[i] -= take * 3;
+
+        if (i + 1 >= c.size()) c.push_back(0);
+        c[i + 1] += take;
+        rem -= take;
+    }
+    cout << (3 * a + extra) << nl;
+}
+int main(){
+    prepare_pow3();
+    int t;
+    cin >> t;
+    while(t--){
+        logic();
+    }
+    return 0;
+}
